a6c54a3efda2a2de86aeaa7fd43d9bb50a60b49c
lumora591-rgb <lumora591@gmail.com>
2026-07-19 17:40:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log("✅ MongoDB Connected"))
+.catch(err => console.log(err));
+
 app.get("/", (req, res) => {
-  res.send("Welcome to Lumora Rewards Backend!");
+    res.send("Welcome to Lumora Rewards Backend!");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
