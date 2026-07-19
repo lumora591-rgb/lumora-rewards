db860b609f388619d2ba75dd44b0cf04018a56e2
lumora591-rgb <lumora591@gmail.com>
2026-07-19 14:17:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -141,3 +141,40 @@ border-radius:10px;
 cursor:pointer;
 font-weight:bold;
 }
+const signupForm = document.getElementById("signupForm");
+
+if (signupForm) {
+
+signupForm.addEventListener("submit", function(event){
+
+event.preventDefault();
+
+const fullname = document.getElementById("fullname").value;
+
+const email = document.getElementById("email").value;
+
+const password = document.getElementById("password").value;
+
+const confirmPassword = document.getElementById("confirmPassword").value;
+
+if(password !== confirmPassword){
+
+alert("Passwords do not match!");
+
+return;
+
+}
+
+localStorage.setItem("fullname", fullname);
+
+localStorage.setItem("email", email);
+
+localStorage.setItem("password", password);
+
+alert("Account created successfully!");
+
+window.location.href = "login.html";
+
+});
+
+    }
