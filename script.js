527c165b9c85030486f7d0c5f003518cafbb0704
lumora591-rgb <lumora591@gmail.com>
2026-07-19 13:38:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const navLinks = document.querySelector(".nav-links");
 menuToggle.addEventListener("click", function(){
     navLinks.classList.toggle("active");
 });
+
+let websiteName = "Lumora Rewards";
+
+let owner = "Odekunle Emmanuel";
+
+let users = 0;
+
+let balance = 0;
+
+console.log(websiteName);
+console.log(owner);
+console.log(users);
+console.log(balance);
